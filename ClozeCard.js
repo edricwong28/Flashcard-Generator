968091e4bc9ab28ececedc0039b931d899eea070
edricwong28 <edricwong28@gmail.com>
2017-07-26 00:18:13 -0700

    Flashcard-Generator

--- ClozeCard.js
+++ ClozeCard.js
@@ -1,21 +1,32 @@
+var exports = require('exports');
+var inquirer = require("inquirer");
+
 function ClozeCard(fullText, cloze){
 	this.fullText = fullText;
 	this.cloze = cloze;
 	this.partial = this.fullText.replace(this.cloze, "...");
 };
 
+var showCard1 = function(){
+	console.log(firstPresidentCloze.partial)
+	inquirer.prompt([
+      {
+        name: "confirm",
+        type: "confirm",
+        message: "Would you like to see the answer?"
+      }
+    ])
+    .then(function(answers){
+    	console.log(firstPresidentCloze.cloze); 
+    }); 
+}
+
 var firstPresidentCloze = new ClozeCard(
     "George Washington was the first president of the United States.", "George Washington");
 
 
-// "George Washington"
-console.log(firstPresidentCloze.cloze); 
-
-// " ... was the first president of the United States.
-console.log(firstPresidentCloze.partial); ""
-
-// "George Washington was the first president of the United States.
-console.log(firstPresidentCloze.fullText); ""
-
 // Should throw or log an error because "oops" doesn't appear in "This doesn't work"
 var brokenCloze = new ClozeCard("This doesn't work", "oops");
+
+module.exports = ClozeCard;
+showCard1();
